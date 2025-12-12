77e301f408fd7dfe79271fd8a951ed558e198d40
zwnewm <zwnewm@gmail.com>
2025-12-12 09:28:06 -0800

    Update KillCilantro.java

--- KillCilantro.java
+++ KillCilantro.java
@@ -2,13 +2,35 @@ public class KillCilantro {
    public static void main(String[] args) {
    
    
+      String result = killCilantro("This is cilantro and cilantro and cilantro woohoo  ");
    
+      System.out.println(result);
    
+   }
    
-   
+   public static String killCilantro(String str) {
+      String newString = "";
+      int index = str.indexOf("cilantro");
+      System.out.println(index);
+      
+      for(int i = 0; i < str.length(); i++) {
+      
+         
+         
+         if(i == index) {
+            String substring = str.substring(0, i);
+            newString = substring + "parsley";
+            
+         
+         }
+         
+         
+      
+      
+      }
+      return newString;
    
    
    }
-
 
 }
